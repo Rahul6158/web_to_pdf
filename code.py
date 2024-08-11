02d751c90a8b5b419c6fdcbdaa491d7fbf07a9df
Rahul6158 <128017087+Rahul6158@users.noreply.github.com>
2024-08-11 17:31:52 +0530

    Update code.py

--- code.py
+++ code.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 # Display a custom message
 st.title("App Under Construction")
-st.subtitle("Webpage to PDF conversion. click here👇🏼👇🏼👇🏼")
+st.header("Webpage to PDF conversion. click here👇🏼👇🏼👇🏼")
 
 # Provide a redirection link
 st.markdown("[webpage-to-pdf-converter](https://pdf-editor-1.streamlit.app/)")
